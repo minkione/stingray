80ce2db6fe31c020187b87ef6e9cb0e4f85c842f
Kazuhiro Ondo <q12877@motorola.com>
2013-02-05 15:52:25 -0600

    RIL and network configuration update for new modem
    - Updating RIL versions to match with new BP release.
    - IP address update in iptables rules. It's for Motorola's debug
      interface only exposed in BP-tools mode.
    - Added APNs which will be excluded from counting data usage.
      Requested from carrier.
    
    Bug: 8140408
    Change-Id: I62ede824139ab5b36239a5d8971332d8ebf260ef

--- ril/wrigley-fetch-mpr.sh
+++ ril/wrigley-fetch-mpr.sh
@@ -5,5 +5,5 @@ case $mprTempDir in
     "") echo "Usage: $0 <destination dir>"; exit 1;;
 esac
 for cmd in "state" "-d logs" "files" "-d panic" "atvc"; do
-    echo "$cmd" | nc 192.168.20.2 3002 | extract-embedded-files -o "$mprTempDir"
+    echo "$cmd" | nc 192.168.157.2 3002 | extract-embedded-files -o "$mprTempDir"
 done
